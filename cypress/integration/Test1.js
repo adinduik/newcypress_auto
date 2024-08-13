b5df688cf0e25cc07f9b19291fafaa8f1a371955
Ikechukwu <adinduikechukwu@gmail.com>
2024-08-13 16:22:05 +0100

    Third Commit

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -1,10 +1,5 @@
 //Sample Test
 
-function Launch (){
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-
-}
-
 
 
 describe ("Cypress Automation Testing",function(){
@@ -12,8 +7,8 @@ describe ("Cypress Automation Testing",function(){
     
 it("Test Case 1", function(){
 
-    //cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    Launch();
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    
     cy.get('.search-keyword').type("Cucumber");
     cy.get('.search-button').click();
 })
